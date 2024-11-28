9e7f39ae34de2194336bfa21bac411e40bb8f522
Tran Manh Son <157039257+manhsontran@users.noreply.github.com>
2024-11-28 11:50:47 +0700

    Update add_category.php

--- add_category.php
+++ add_category.php
@@ -1,9 +1,9 @@
 <?php
-include 'db.php'; // Kết nối cơ sở dữ liệu
+include 'db.php'; 
 
 $category_name = $_POST['category_name'];
 
-// Chuẩn bị câu truy vấn để thêm category
+
 $sql = "INSERT INTO Categories (category_name) VALUES (?)";
 $stmt = $conn->prepare($sql);
 $stmt->bind_param("s", $category_name);
